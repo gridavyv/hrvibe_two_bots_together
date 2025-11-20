368323ab2d38a1d7097921608f6cd2700625773b
Grigory Davydov <gdavidov22@gmail.com>
2025-11-20 12:16:09 +0300

    applicant_bot: modified constants import in data_service.py

--- applicant_bot/services/data_service.py
+++ applicant_bot/services/data_service.py
@@ -13,10 +13,7 @@ logger = logging.getLogger(__name__)
 
 
 
-from services.constants import (
-    APPLICANT_BOT_DATA_DIR,
-    APPLICANT_BOT_RECORDS_FILENAME,
-    )
+from services.constants import APPLICANT_BOT_DATA_DIR, APPLICANT_BOT_RECORDS_FILENAME
 
 
 # ****** METHODS with TAGS: [get_data] ******
